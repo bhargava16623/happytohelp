2c65a585e83e9f5fff5a24e73fc396d7a7c2e438
bhargava16623 <105556187+bhargava16623@users.noreply.github.com>
2024-07-13 15:38:10 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import BasketBallPlayerCard from './Components/BasketBallPlayerCard';
 
 function App() {
   return (
+  <>
+     <div id="ball-shape">
+                <div id="ball-1"></div>
+                <div id="ball-2"></div>
+                <div id="ball-3"></div>
+     </div>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <BasketBallPlayerCard name="LeBron James" 
+       image="https://upload.wikimedia.org/wikipedia/commons/7/7a/LeBron_James_%2851959977144%29_%28cropped2%29.jpg" 
+       position="Forward" 
+       stats={{ pointsPerGame: 25.4, assistsPerGame: 7.1,reboundsPerGame: 10.5 }}/>
     </div>
+  </>
   );
 }
 
